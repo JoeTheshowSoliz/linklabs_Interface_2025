21eb8e87d11e3cbf0a98d403e96b65ef38ad5995
Dominik Pilat <dominik.pilat@link-labs.com>
2025-01-31 23:50:47 -0500

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://geotab.link-labs.com',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
